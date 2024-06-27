794b9ab62c55f8833214e0bb663a3ff98cb14f54
Ganpat Sundesha <ganpatsundesha47@gmail.com>
2024-06-27 14:46:09 +0530

    localstorage and add todo done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css'
 
 const App = () => {
+
+    const localdata = localStorage.getItem("todos")
+    const todoData = JSON.parse(localdata) || []
+
     const [formValue, setFormValue] = useState({
         name: "",
         email: "",
         discription: "",
     });
-    const [formError, setFormError] = useState("")
-
+    const [formError, setFormError] = useState("");
+    const [todos, setTodos] = useState([])
 
     const handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         setFormValue((prev) => ({ ...prev, [name]: value }));
-    };
 
+    };
 
     const formSubmit = (event) => {
         event.preventDefault();
@@ -24,8 +28,17 @@ const App = () => {
         }
         else {
             setFormError("")
+            setTodos((prev) => ([...prev, formValue]))
         }
     };
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos])
+
+    useEffect(() => {
+        setTodos(todoData)
+    }, [window])
 
     return (
         <div className="sectionForm">
@@ -37,6 +50,24 @@ const App = () => {
                     <input type="text" placeholder="Discription" value={formValue.discription || ""} name="discription" onChange={handleChange} />
                     <input type="submit" value="Submit" />
                 </form>
+                <div className="d-flex">
+                    {
+                        todos.length > 0 && todos.map((elem, index) => {
+                            const { name, email, discription } = elem
+                            return (
+                                <div className="todoCard" key={index}>
+                                    <div>{name}</div>
+                                    <div>{email}</div>
+                                    <div>{discription}</div>
+                                    
+                                </div>
+                            )
+                        })
+                    }
+                    {
+                        todos.length <= 0 && todos === null && <h1>No Found</h1>
+                    }
+                </div>
             </div>
         </div>
     );
