db3ccc6b689dbc3a6a54e26d427f41d079e2dda9
Ganpat Sundesha <ganpatsundesha47@gmail.com>
2024-06-27 11:20:28 +0530

    initial commit with form code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [formValue, setFormValue] = useState({
+        name: "",
+        email: "",
+        discription: "",
+    });
+    const [formError, setFormError] = useState("")
+
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+        setFormValue((prev) => ({ ...prev, [name]: value }));
+    };
+
+
+    const formSubmit = (event) => {
+        event.preventDefault();
+        if (formValue.name === "" || formValue.email === "" || formValue.discription === "") {
+            setFormError("Something is missing")
+        }
+        else {
+            setFormError("")
+        }
+    };
+
+    return (
+        <div className="sectionForm">
+            <div className="container">
+                <form onSubmit={formSubmit}>
+                    {formError && <p>{formError}</p>}
+                    <input type="text" placeholder="Name" value={formValue.name || ""} name="name" onChange={handleChange} />
+                    <input type="email" placeholder="Email" required value={formValue.email || ""} name="email" onChange={handleChange} />
+                    <input type="text" placeholder="Discription" value={formValue.discription || ""} name="discription" onChange={handleChange} />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </div>
+    );
+};
 
 export default App;
