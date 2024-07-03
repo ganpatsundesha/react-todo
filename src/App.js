b27e81b7433023aa6da7c16fcea07515ea77304b
Ganpat Sundesha <ganpatsundesha47@gmail.com>
2024-07-03 14:52:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ const App = () => {
 
     const localdata = localStorage.getItem("todos")
     const todoData = JSON.parse(localdata) || []
-
     const [formValue, setFormValue] = useState({
         name: "",
         email: "",
         discription: "",
+        endTime: "",
+        // status: "pending",
     });
     const [formError, setFormError] = useState("");
     const [todos, setTodos] = useState([])
@@ -19,7 +20,9 @@ const App = () => {
     const [filteredUsers, setFilteredUsers] = useState()
 
     const time = new Date()
-    const fullTime = `${time.getHours().toString().padStart(2, '0')}:${time.getMinutes().toString().padStart(2, '0')}-${time.getDate().toString().padStart(2, '0')}/${(time.getMonth() + 1).toString().padStart(2, '0')}/${time.getFullYear()}`
+    const fullTime = `${time.getHours().toString().padStart(2, '0')}:${time.getMinutes().toString().padStart(2, '0')}`
+    const curDate = `${time.getDate().toString().padStart(2, '0')}/${(time.getMonth() + 1).toString().padStart(2, '0')}/${time.getFullYear()}`
+    const miliSecondTime = time.getTime()
 
     const handleChange = (e) => {
         const name = e.target.name;
@@ -29,7 +32,7 @@ const App = () => {
 
     const formSubmit = (event) => {
         event.preventDefault();
-        if (formValue.name === "" || formValue.email === "" || formValue.discription === "") {
+        if (formValue.name.trim() === "" || formValue.email === "" || formValue.discription.trim() === "") {
             setFormError("Something is missing")
         }
         else if (isEdited) {
@@ -39,7 +42,7 @@ const App = () => {
                         setIsEdited(false)
                         setEditedId(null)
                         setFormValue("")
-                        return { ...curItem, name: formValue.name, email: formValue.email, discription: formValue.discription, time: fullTime, edit: true };
+                        return { ...curItem, name: formValue.name, email: formValue.email, discription: formValue.discription, endTime: formValue.endTime, time: fullTime, date: curDate, edit: true };
                     }
                     else {
                         return curItem;
@@ -49,7 +52,7 @@ const App = () => {
         }
         else {
             setFormError("")
-            setTodos((prev) => ([...prev, { ...formValue, id: Date.now(), time: fullTime, edit: false }]))
+            setTodos((prev) => ([...prev, { ...formValue, id: Date.now(), edit: false, time: fullTime, date: curDate, curTime: miliSecondTime }]))
             setFormValue("")
         }
     };
@@ -93,6 +96,30 @@ const App = () => {
         setFilteredUsers(filterdItem)
     }
 
+    // useEffect(() => {
+
+    //     filteredUsers?.length > 0 && filteredUsers.map((element) => {
+    //         if (element.status === "pending") {
+    //             const elemTime = new Date(element.endTime).getTime() / 1000;
+    //             const newTime = new Date().getTime() / 1000;
+    //             const removalTime = (elemTime - newTime).toFixed() * 1000
+    //             setTimeout(() => {
+    //                 const restItem = todos?.length > 0 && todos.filter((elem) => {
+    //                     return elem.id !== element.id
+    //                 })
+    //                 console.log(restItem);
+    //                 setTodos(restItem);
+    //             }, removalTime)
+    //             if (removalTime <= 0) {
+    //                 const restItem = todos?.length > 0 && todos.filter((elem) => {
+    //                     return elem.id !== element.id
+    //                 })
+    //                 setTodos(restItem || []);
+    //             }
+    //         }
+    //     })
+    // }, [todos])
+
     useEffect(() => {
         localStorage.setItem("todos", JSON.stringify(todos))
         setFilteredUsers(todos)
@@ -110,6 +137,8 @@ const App = () => {
                     <input type="text" placeholder="Name" value={formValue.name || ""} name="name" onChange={handleChange} />
                     <input type="email" placeholder="Email" required value={formValue.email || ""} name="email" onChange={handleChange} />
                     <input type="text" placeholder="Discription" value={formValue.discription || ""} name="discription" onChange={handleChange} />
+                    {/* <label>Task End Time</label> */}
+                    {/* <input type="datetime-local" value={formValue.endTime || ""} name="endTime" onChange={handleChange} /> */}
                     <input type="submit" value="Submit" />
                 </form>
                 {
@@ -118,14 +147,14 @@ const App = () => {
                 <div className="d-flex">
                     {
                         filteredUsers?.length > 0 ? filteredUsers.map((elem, index) => {
-                            const { name, email, discription, id, time, edit, pin } = elem
+                            const { name, email, discription, id, time, date, edit, pin } = elem
                             return (
-                                <div className="todoCard" key={index}>
+                                <div className={`todoCard`} key={index} >
                                     {edit && <span>{edit ? "Edited" : ""}</span>}
                                     <div>{name}</div>
                                     <div>{email}</div>
                                     <div>{discription}</div>
-                                    <div>{time} <span>{edit && "(Last Edited Time)"}</span></div>
+                                    <div>{time} - {date} <span>{edit && "(Last Edited Time)"}</span></div>
                                     {pin && <div className="pin">Pin</div>}
                                     <div className="d-flex">
                                         <button onClick={editTask(id)}>Edit</button>
@@ -139,7 +168,6 @@ const App = () => {
                                 todos.length > 0 ? <h3>Result Not Found</h3> : <h3>Please Add Your Tasks</h3>
                             }
                         </>
-
                     }
                     {
                         todos.length <= 0 && todos === null && <h1>No Found</h1>
